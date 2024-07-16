728ec7e3078d0fad2555ec05bcb587e97464acde
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-07-16 10:36:43 -0400

    Add files via upload

---
+++ gen_keys.py
@@ -0,0 +1,38 @@
+from web3 import Web3
+import eth_account
+import os
+
+def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
+    """
+    Generate a stable private key
+    challenge - byte string
+    keyId (integer) - which key to use
+    filename - filename to read and store mnemonics
+
+    Each mnemonic is stored on a separate line
+    If fewer than (keyId+1) mnemonics have been generated, generate a new one and return that
+    """
+
+    w3 = Web3()
+
+    msg = eth_account.messages.encode_defunct(challenge)
+
+	#YOUR CODE HERE
+    private_key = '0x495dcd5c30f204d649c6f3a5fd66225001122ddca3f5a578fa9dd4f31cccce1c'
+    acct = w3.eth.account.from_key(private_key)
+    eth_addr = acct.address
+
+    signed_message = acct.sign_message(msg)
+
+
+
+    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
+
+    #return sig, acct #acct contains the private key
+    return signed_message.signature.hex(), eth_addr
+
+if __name__ == "__main__":
+    for i in range(4):
+        challenge = os.urandom(64)
+        sig, addr= get_keys(challenge=challenge,keyId=i)
+        print( addr )
